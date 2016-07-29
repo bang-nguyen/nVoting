510f996de62cda47ec8eeac892b0c2d03b43599c
Thang Le <thang.le@niteco.se>
2016-07-29 13:48:08 +0700

    Homepage Binding (exclude paging and check outdate...)

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,8 +1,12 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-  function ($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication', 'VotedefinitionsService',
+  function ($scope, Authentication, VotedefinitionsService) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
-  }
+
+    $scope.find = function () {
+      $scope.votedefinitions = VotedefinitionsService.query();
+    };
+  }  
 ]);
